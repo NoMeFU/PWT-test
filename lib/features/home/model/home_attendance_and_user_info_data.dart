00a9571db1b6932c64884018d51460ac796b37a3
NoMeFU <146742289+VilorsWeb@users.noreply.github.com>
2026-05-13 17:57:42 +0300

    Fix: Added email field to User model to resolve compilation error caused by tester account bypass in navigation.dart

--- lib/features/home/model/home_attendance_and_user_info_data.dart
+++ lib/features/home/model/home_attendance_and_user_info_data.dart
@@ -99,12 +99,14 @@ class Attendance {
 class User {
   final int? id;
   final String? name;
+  final String? email;
   final String? avatar;
   final int? workingDays;
 
   User({
     this.id,
     this.name,
+    this.email,
     this.avatar,
     this.workingDays,
   });
@@ -116,6 +118,7 @@ class User {
   factory User.fromJson(Map<String, dynamic> json) => User(
     id: json["id"],
     name: json["name"],
+    email: json["email"],
     avatar: json["avatar"],
     workingDays: json["working_days"],
   );
@@ -123,6 +126,7 @@ class User {
   Map<String, dynamic> toJson() => {
     "id": id,
     "name": name,
+    "email": email,
     "avatar": avatar,
     "working_days": workingDays,
   };
